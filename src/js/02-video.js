c9a061d327e1e8c088191440be484375377e6142
AlexSBolotov <122620169+AlexSBolotov@users.noreply.github.com>
2023-03-26 16:07:18 +0300

    task02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+// console.log(iframe);
+
+player.on('timeupdate', throttle(currentTime, 1000));
+function currentTime({ seconds }) {
+  //   console.log(seconds);
+  localStorage.setItem('videoplayer-current-time', `${seconds}`);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
