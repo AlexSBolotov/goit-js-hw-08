3164beb8001a69f3bf76c4060b63077c98a2bd8f
AlexSBolotov <122620169+AlexSBolotov@users.noreply.github.com>
2023-03-27 01:41:21 +0300

    task03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,14 +2,14 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-
 const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 // console.log(iframe);
 
 player.on('timeupdate', throttle(currentTime, 1000));
 function currentTime({ seconds }) {
   //   console.log(seconds);
-  localStorage.setItem('videoplayer-current-time', `${seconds}`);
+  localStorage.setItem(LOCALSTORAGE_KEY, `${seconds}`);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,66 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const formData = {};
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(collectFormData, 500));
+
+fillEmailOnRefresh(LOCALSTORAGE_KEY);
+fillMessageOnRefresh(LOCALSTORAGE_KEY);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  e.currentTarget.reset();
+  console.log(load(LOCALSTORAGE_KEY));
+
+  remove(LOCALSTORAGE_KEY);
+}
+
+function collectFormData(e) {
+  formData[e.target.name] = e.target.value;
+
+  //   console.log(formData);
+  save(LOCALSTORAGE_KEY, formData);
+}
+
+function fillEmailOnRefresh(key) {
+  const dataToFill = load(key);
+  //   form.email.value = dataToFill.email || '';
+  if (dataToFill.email) {
+    form.email.value = dataToFill.email;
+  }
+}
+function fillMessageOnRefresh(key) {
+  const dataToFill = load(key);
+  //   form.message.value = dataToFill.message || '';
+  if (dataToFill.message) {
+    form.message.value = dataToFill.message;
+  }
+}
+function save(key, value) {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function load(key) {
+  try {
+    const savedData = localStorage.getItem(key);
+    return savedData === null ? undefined : JSON.parse(savedData);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function remove(key) {
+  try {
+    localStorage.removeItem(key);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
